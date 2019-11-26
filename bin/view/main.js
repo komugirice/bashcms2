9416df72fe58a41409f722a8009c45ac4e3508e2
komugirice <iys60111118082@gmail.com>
2019-11-26 21:28:02 +0900

    keyword

--- bin/view/main.js
+++ bin/view/main.js
@@ -1,5 +1,6 @@
 window.onload = function() {
   lastArticles(10);
+  linkKeywords();
 }
 
 function lastArticles(num) {
@@ -15,6 +16,23 @@ function lastArticles(num) {
     //console.log( httpReq.responseText );
   }
   var url = "./last_articles.cgi?num=" + num;
+  httpReq.open("GET", url, true);
+  httpReq.send(null);
+}
+
+function linkKeywords() {
+  var httpReq = new XMLHttpRequest();
+  httpReq.onreadystatechange = function() {
+    if(httpReq.readyState != 4 || httpReq.status != 200) {
+      console.log("readyState:" + httpReq.readyState);
+      console.log("status:" + httpReq.status);
+      return;  
+    }
+
+    document.getElementById("keywords").innerHTML = httpReq.responseText;
+    //console.log( httpReq.responseText );
+  }
+  var url = "./link_keywords.cgi?keywords=" + encodeURIComponent(word);
   httpReq.open("GET", url, true);
   httpReq.send(null);
 }
